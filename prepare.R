560302c19002fa936b7c6ca252f6954461847553
Ildiko Czeller <iczeller@emarsys.com>
2016-09-19 20:47:18 +0200

    change to data:table

--- prepare.R
+++ prepare.R
@@ -1,4 +1,7 @@
 library(magrittr)
+library(data.table)
+
 cdfk_pontok <- read.csv("9-cdfk-helyi-pontozo.csv", stringsAsFactors = F) %>% 
     tidyr::separate(Tagok, paste0('tag_', 1:3), sep = '\n') %>% 
-    tidyr::separate(`Évf.`, paste0('Evfolyam_', 1:3), sep = '\n')
+    tidyr::separate(`Évf.`, paste0('Evfolyam_', 1:3), sep = '\n') %>% 
+    data.table()
