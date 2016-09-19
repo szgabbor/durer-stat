7ce3ffe24df76e94c37934de54e95317daea8f1f
Ildiko Czeller <iczeller@emarsys.com>
2016-09-19 20:15:56 +0200

    stringasFactors to read

--- prepare.R
+++ prepare.R
@@ -1,5 +1,4 @@
 library(magrittr)
-cdfk_pontok <- read.csv("9-cdfk-helyi-pontozo.csv") %>% 
+cdfk_pontok <- read.csv("9-cdfk-helyi-pontozo.csv", stringsAsFactors = F) %>% 
     tidyr::separate(Tagok, paste0('tag_', 1:3), sep = '\n') %>% 
-    tidyr::separate(`Évf.`, paste0('Evfolyam_', 1:3), sep = '\n') %>% 
-    View()
+    tidyr::separate(`Évf.`, paste0('Evfolyam_', 1:3), sep = '\n')
