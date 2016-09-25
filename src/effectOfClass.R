aa9b17fc12327d86a6091c1429972fa20598fb5d
Ildiko Czeller <iczeller@emarsys.com>
2016-09-25 16:48:44 +0200

    select measure cols in melt

--- src/effectOfClass.R
+++ src/effectOfClass.R
@@ -1,10 +1,10 @@
 classDataForTeams <- function(scores_in_category) {
     scores_in_category %>% 
-        .[,
-          .SD,
-          .SDcols = c('team', paste0('class_', 1:3), 'total')
-          ] %>% 
-        melt(id.vars = c('team', 'total'), value.name = 'class') %>% 
+        melt(
+            id.vars = c('team', 'total'),
+            measure.vars = paste0('class_', 1:3),
+            value.name = 'class'
+        ) %>% 
         .[, lowest_class := min(as.integer(class)), by = 'team'] %>% 
         .[, num_member_in_lower_class := sum(class == lowest_class), by = 'team'] %>% 
         .[, .(total = mean(total)), by = c('team', 'num_member_in_lower_class')]
